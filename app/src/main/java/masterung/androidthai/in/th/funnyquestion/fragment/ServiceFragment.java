741f1fdb276875b02b1009b533a0bbaf308f61a5
masterUNG <phrombutr@gmail.com>
2018-08-13 15:09:01 +0700

    finish 13aug

--- app/src/main/java/masterung/androidthai/in/th/funnyquestion/fragment/ServiceFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/funnyquestion/fragment/ServiceFragment.java
@@ -1,8 +1,54 @@
 package masterung.androidthai.in.th.funnyquestion.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class ServiceFragment {
+import masterung.androidthai.in.th.funnyquestion.R;
+import masterung.androidthai.in.th.funnyquestion.utility.MyConstant;
 
+public class ServiceFragment extends Fragment{
 
+    private int indexAnInt = 0;
+
+    public static ServiceFragment serviceInstance(int index) {
+
+        ServiceFragment serviceFragment = new ServiceFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("Index", index);
+        serviceFragment.setArguments(bundle);
+        return serviceFragment;
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        indexAnInt = getArguments().getInt("Index");
+
+//        Show Title
+        showTitle();
+
+    }
+
+    private void showTitle() {
+        TextView textView = getView().findViewById(R.id.txtShowTitle);
+        MyConstant myConstant = new MyConstant();
+        String[] strings = myConstant.getUnitTitleStrings();
+        textView.setText(strings[indexAnInt]);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_service,container, false);
+        return view;
+    }
 }   // Main Class
---
+++ app/src/main/java/masterung/androidthai/in/th/funnyquestion/utility/MyConstant.java
@@ -0,0 +1,17 @@
+package masterung.androidthai.in.th.funnyquestion.utility;
+
+public class MyConstant {
+
+    private String[] unitTitleStrings = new String[]{
+            "Unit1 for Basic",
+            "Unit2",
+            "Unit3",
+            "Unit4",
+            "Unit5",
+            "Unit6"
+    };
+
+    public String[] getUnitTitleStrings() {
+        return unitTitleStrings;
+    }
+}
